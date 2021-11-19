b2cba412255d8f2f9accc6202b03db89c5b20bcd
lockedinbasement <dariusz.strych.lib@gmail.com>
2021-11-19 21:10:21 +0100

    grade 5

--- S5P5/App.js
+++ S5P5/App.js
@@ -1,59 +1,25 @@
 class Form extends React.Component {
-    state = { 
-       city: "Warsaw",
-       text: "",
-       isLiked: true,
-       number: 0,
-    }
+   render()
+   {
+       state = {
+            options: ["1","2","3"],
+            option: null,
+       }
 
-    handleChange = e =>{
-        console.log(e.target.type);
+       handleShowOption = () => {
+           const index = Math.floor(Math.Random() * this.state.options.lenght);
+           this.setState({
+               option: this.state.options[index]
+           })
+       }
 
-        if(e.target.type === "checkbox"){
-            this.setState({
-                [e.target.name]: e.target.checked
-            })
-        }
-        else{
-            this.setState({
-                [e.target.name]: e.target.value
-            })
-        }
-
-    }
-
-    render() { 
-        return (  
-            <div>
-                <label>
-                    Podaj miasto
-                    <input name="city" value={this.state.city} onChange={this.handleChange} type="text"/>
-                </label>
-                <br/>
-                <label>
-                    Napisz coś o tym mieście
-                    <textarea name="text" value={this.state.text} onChange={this.handleChange}></textarea>
-                </label>
-                <br/>
-                <label>
-                    Czy lubisz to miasto?
-                    <input name="isLiked" type="checkbox" checked={this.isLiked} onChange={this.handleChange} ></input>
-                </label>
-                <br/>
-                <label>
-                    Ile razy byliście w tym mieście?
-                    <select name="number" value={this.state.number} onChange={this.handleChange}>
-                        <option value="0">0</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="more">więcej</option>
-                    </select>
-                </label>
-            </div>
-
-        )
-    }
+       return(
+           <div>
+           <button onClick={this.handleShowOption}>Zobacz wrozbe</button>
+           {this.state.option ? <h1>this.state.option</h1> : null}
+           </div>
+       )
+   }
 }
 
 ReactDOM.render(<Form/>, document.getElementById('root'))
