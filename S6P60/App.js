875c703bd4acb66cf0f4dd13833cd6ab7fa4c722
lockedinbasement <dariusz.strych.lib@gmail.com>
2021-11-26 11:59:47 +0100

    chapter6 start

---
+++ S6P60/App.js
@@ -0,0 +1,58 @@
+class Clicker extends React.Component {
+
+    state = {
+        number1:0,
+        number2:0,        
+        number3:0,
+    }
+
+    handleClick1 = () =>{
+        this.setState({
+            number1: this.state.number1 + 1,
+        })
+        console.log(this.state.number1 + " w metodzie handleClick1");
+    }
+
+    handleClick2 = () =>{
+        this.setState({
+            number2: this.state.number2 + 1,
+        })
+        this.setState({
+            number2: this.state.number2 + 1,
+        })
+        console.log(this.state.number2 + " w metodzie handleClick2");
+    }
+
+    handleClick3 = () =>{
+        this.setState((prevState) => ({
+            number3: prevState.number3 + 1,
+        }))
+        this.setState((prevState) => ({
+            number3: prevState.number3 + 1,
+        }))
+        this.setState((prevState) => ({
+            number3: prevState.number3 + 1,
+        }))
+        console.log(prevState.number3 + " w metodzie handleClick3");
+    }
+
+    render(){
+        //console.log(this.state.number1 + " w metodzie render");
+        //console.log(this.state.number2 + " w metodzie render");
+        console.log(this.state.number3 + " w metodzie render");
+        return(
+            <div>
+                <button onClick={this.handleClick1}>Podbij o 1</button>
+                <h1>{this.state.number1}</h1>'
+                <button onClick={this.handleClick2}>Podbij o 2</button>
+                <h1>{this.state.number2}</h1>
+                <button onClick={this.handleClick3}>Podbij o 3</button>
+                <h1>{this.state.number3}</h1>
+            </div>
+        )
+    }
+
+
+}
+
+ReactDOM.render(<Clicker/>, document.getElementById('root'))
