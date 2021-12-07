a45b08d2fccbd11a482709d640863a7a30bfe74c
lockedinbasement <dariusz.strych.lib@gmail.com>
2021-12-07 14:12:10 +0100

    proj65

---
+++ S6P65/App.js
@@ -0,0 +1,26 @@
+class Clock extends React.Component {
+
+    state = {
+        tinme: 0,
+    }
+
+    handleMultiplication = () => 
+    {
+
+    }
+
+    componentDidUpdate()
+    {
+
+    }
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Clock/>, document.getElementById('root'))
