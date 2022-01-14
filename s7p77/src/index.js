212a649459f086008a2140246f8ea0c7700eec97
lockedinbasement <dariusz.strych.lib@gmail.com>
2022-01-14 13:39:25 +0100

    p90

--- s7p77/src/index.js
+++ s7p77/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App86 from './proj86/components/App86';
+import App90 from './proj90/components/App90';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App86 />, document.getElementById('root'));
+ReactDOM.render(<App90 />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ s7p77/src/proj90/components/App90.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App90.css';
+
+class App90 extends Component {
+
+  state = {
+  }
+
+  componentDidMount(){
+  }
+
+  render() {
+    return (
+      <div>
+       hello!
+      </div>
+    );
+  }
+}
+
+export default App90;
